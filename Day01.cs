ccef5b05609a48965f483f24595a3ff54b76b12b
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2020-12-01 09:32:35 +0100

    Day 01

---
+++ Day01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    public class Day01
+    {
+        private static IEnumerable<int> ParsedInput()
+        {
+            return
+                from line in File.ReadAllLines("Day01.txt")
+                select int.Parse(line);
+        }
+
+        public static string Part1()
+        {
+            var numbers = ParsedInput();
+
+            return (from number1 in numbers
+                from number2 in numbers
+                where number1 + number2 == 2020
+                select (number1 * number2).ToString()).First();
+        }
+
+        public static string Part2()
+        {
+            var numbers = ParsedInput();
+
+            return (from number1 in numbers
+                    from number2 in numbers
+                    from number3 in numbers
+                    where number1 + number2 + number3 == 2020
+                    select (number1 * number2 * number3).ToString()).First();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Day  1 - Part1: {Day01.Part1()} Part2: {Day01.Part2()}");
+        }
+    }
+}
