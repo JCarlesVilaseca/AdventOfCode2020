9bc5a9d489e8cbefcc3fac3a9400a0c079f3841b
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2020-12-03 20:28:04 +0100

    Day 03

--- Day03.cs
+++ Day03.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode2020
 {
@@ -8,34 +9,34 @@ namespace AdventOfCode2020
     {
         public static void Solve()
         {
-            var input = from line in File.ReadAllLines("Day03.txt")
-                           select line;
-
-            var length = input.ElementAt(0).Length;
-
-            Console.WriteLine("Lentgh=" + length);
-
-            var full = string.Join("", input);
-
-            var part1 = from line in full.Select((chr, pos) => new {  chr, pos })
-                        
-                        where ((line.pos +1 ) % (length + 3)) == 0 && line.chr == '#'
+            var lines = from line in File.ReadLines("Day03.txt")
                         select line;
-            /*
-            var part2 = from policy in policies
-                        let first = policy.Text[policy.Min - 1] == policy.Chr
-                        let second = policy.Text[policy.Max - 1] == policy.Chr
-                        where first ^ second
-                        select policy;
-            */
 
-            part1.ToList().ForEach(x =>
-            {
+            var slope31 = Trees(lines, 3, 1);
 
-                Console.WriteLine(x);
-            });
+            var slope11 = Trees(lines, 1, 1);
+            var slope51 = Trees(lines, 5, 1);
+            var slope71 = Trees(lines, 7, 1);
+            var slope12 = Trees(lines, 1, 2);
 
-            Console.WriteLine($"Day  2 - Part1: {part1.Count()}\t\tPart 2: {0}");
+            var part1 = slope31;
+            var part2 = slope11 * slope31 * slope51 * slope71 * slope12;
+
+            Console.WriteLine($"Day  2 - Part1: {part1}\t\tPart 2: {part2}");
+        }
+
+        private static int Trees(IEnumerable<string> lines, int right, int down)
+        {
+            var found = from line in lines
+                            .Skip(down)
+                            .Select((map, row) => new { map, row })
+                        where line.row % down == 0
+                        let pos = ((line.row/down + 1) * right) % line.map.Length
+                        let map = line.map[pos]
+                        where map == '#'
+                        select line;
+            
+            return found.Count();
         }
     }
 }
